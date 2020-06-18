b2b77978a835fd11062304d27d3773d6e61ca899
codefactor-io <support@codefactor.io>
2020-06-18 18:29:11 +0000

    [CodeFactor] Apply fixes

--- src/main/webapp/ts/app.ts
+++ src/main/webapp/ts/app.ts
@@ -6,12 +6,12 @@ function loadMatchHistory() {
 //   let num = document.getElementById("number").value;
   fetch('/matches')
   .then(response => response.json())
-  .then((matches) => 
+  .then((matches) =>
   {
     let matchListElement = document.getElementById('match-history');
     matchListElement.innerHTML = "";
         
-    matches.forEach((match) => 
+    matches.forEach((match) =>
     {
       matchListElement.appendChild(createMatchElement(match));
     })
