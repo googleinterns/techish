5b433467b8fcbd7dc8a9f36dd4fa08e45f95d9f7
Hadley <yeldah630@gmail.com>
2020-06-19 21:01:42 +0000

    add testing files

---
+++ src/test/java/com/google/sps/NonPersistentMatchRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.google.sps;
+
+import com.google.sps.data.NonPersistentMatchRepository;
+import java.util.Arrays;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** */
+@RunWith(JUnit4.class)
+public final class NonPersistentMatchRepositoryTest {
+  
+}
