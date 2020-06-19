b48712ee421f50b59fb5d17d1e9686cabc9f78c5
Hadley <yeldah630@gmail.com>
2020-06-19 21:53:00 +0000

    finish nonpersistentmatchrepo tests

--- src/test/java/com/google/sps/NonPersistentMatchRepositoryTest.java
+++ src/test/java/com/google/sps/NonPersistentMatchRepositoryTest.java
@@ -1,5 +1,8 @@
 package com.google.sps;
 
+import com.google.sps.data.Match;
+import com.google.sps.data.User;
+import com.google.sps.data.NonPersistentMatchRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,8 +11,43 @@ import org.junit.runners.JUnit4;
 /** */
 @RunWith(JUnit4.class)
 public final class NonPersistentMatchRepositoryTest {
+
+  private static final NonPersistentMatchRepository TEST_DATA_REPO = new NonPersistentMatchRepository();
+  private static final NonPersistentMatchRepository EMPTY_REPO = new NonPersistentMatchRepository();
+  private static final User TEST_USER = TEST_DATA_REPO.addTestData();  
+  private static final User USER_A = new User("User A");
+  private static final Match MATCH_A = new Match("Match A");
+
+
   @Test
-  public void emptyTest() {
-    Assert.assertFalse(1 == 2);
+  public void addTestDataTest() {
+    String expected = "{Test User=[John, Sarah, David, Kate]}";
+    Assert.assertEquals(expected, TEST_DATA_REPO.toString());
+  }
+
+  @Test
+  public void addMatchTest() {
+    EMPTY_REPO.addMatch(USER_A, MATCH_A);
+    String expected = "{User A=[Match A]}";
+    Assert.assertEquals(expected, EMPTY_REPO.toString());
+  }
+
+  @Test
+  public void removeMatchThatExists() {
+      try {
+      EMPTY_REPO.removeMatch(USER_A, MATCH_A);
+      String expected = "{User A=[]}";
+      Assert.assertEquals(expected, EMPTY_REPO.toString());
+      } catch (Exception e) {
+      }
+  }
+
+  @Test
+  public void removeMatchThatDoesNotExist() {
+       try {
+      EMPTY_REPO.removeMatch(USER_A, MATCH_A);
+      Assert.fail("Expected exception in removeMatchThatDoesNotExist()");
+      } catch (Exception e) {
+      }
   }
 }
