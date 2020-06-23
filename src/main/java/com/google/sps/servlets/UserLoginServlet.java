c3b2dc4aa177d3fd6e314be62c7cc0d1f86a76f8
samuelsandoval1 <samuelsandoval@google.com>
2020-06-23 11:44:52 -0700

    responded to some comments

--- src/main/java/com/google/sps/servlets/UserLoginServlet.java
+++ src/main/java/com/google/sps/servlets/UserLoginServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
     name = "UserAPI",
     description = "UserAPI: Login / Logout with UserService",
     urlPatterns = "/userapi")
-public class userApi extends HttpServlet {
+public class UserLoginServlet extends HttpServlet {
 
   /*This method uses the UsersAPI to send a Get Request and see if the user is logged in,
   and is expected to return a string with the url and boolean value all in a Json string */
@@ -24,6 +24,12 @@ public class userApi extends HttpServlet {
     UserService userService = UserServiceFactory.getUserService();
     Boolean isLoggedIn = userService.isUserLoggedIn();
 
+    /* This checks to see if user is logged in and then decides whether to store
+      a logged out url or log in url. A log out url is needed when a user is 
+      logged in so that they can log out of their user account on the platform.
+      The same goes with being logged out, we make the url shown be the log in 
+      url so a user can log in to their account and use the platform.
+    */
     String url =
         isLoggedIn
             ? userService.createLogoutURL("/index.html")
