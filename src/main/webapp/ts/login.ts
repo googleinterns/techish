7dfeb3269703624954f8d43a830c48fd90a7eaaf
Samuel Sandoval <sjsandoval1000@gmail.com>
2020-06-23 21:30:11 +0000

    made changes to respond to pr

--- src/main/webapp/ts/login.ts
+++ src/main/webapp/ts/login.ts
@@ -1,27 +1,36 @@
-// interface Value {}
-window.onload = () => {
-  loadHome();
+
+interface authInfo {
+    loginUrl: string;
+    logoutUrl: string;
 };
 
 async function loadHome() {
   const logging = document.getElementById('log-status-section');
   const link = document.getElementById('login-link');
 
-  const logStatus = await getLogStatus();
 
+    //set up function to set login/logout link based on which string is non empty
   if (link && logging) {
-    link.setAttribute('href', logStatus.Url);
-    logging.style.display = 'block';
+  
     if (logStatus.Bool) {
       link.innerHTML = 'Logout';
+      logStatus.loggedOutUrl = logStatus.url;
     } else {
       link.innerHTML = 'Login';
     }
+    link.setAttribute('href', logStatus.Url);
+    logging.style.display = 'block';
   }
 }
 
-async function getLogStatus() {
+async function getLogStatus(currentStatus: authInfo) {
   const response = await fetch('/userapi');
   const isLoggedIn = await response.json();
   return isLoggedIn;
+}
+const currentStatus;
+const logStatus = await getLogStatus(currentStatus);
+
+window.onload = () => {
+  loadHome();
 }
