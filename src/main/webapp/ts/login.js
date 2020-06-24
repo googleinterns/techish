fbb2b69b9765d4f1a25bb7f09b958af5f6fff6e0
samuelsandoval1 <samuelsandoval@google.com>
2020-06-24 16:54:46 -0700

    respond to PR

--- src/main/webapp/ts/login.js
+++ src/main/webapp/ts/login.js
@@ -34,13 +34,14 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+//function that loads the home page for logging in and logging out
 function loadHome() {
     return __awaiter(this, void 0, void 0, function () {
         var logging, link, logStatus;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
-                    logging = document.getElementById('log-status-section');
+                    logging = document.getElementById('login-status-section');
                     link = document.getElementById('login-link');
                     return [4 /*yield*/, getLogStatus()];
                 case 1:
