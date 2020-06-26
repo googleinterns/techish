1cee0bfd6b7571f75068cb3d2de663d268652504
samuelsandoval1 <samuelsandoval@google.com>
2020-06-26 23:12:45 +0000

    fixed merge conflicts

--- src/test/java/com/google/sps/UserLoginTest.java
+++ src/test/java/com/google/sps/UserLoginTest.java
@@ -1,12 +1,7 @@
 package com.google.sps;
 
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.verify;
 
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
-import com.google.appengine.api.users.dev.LocalUserService;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import com.google.appengine.tools.development.testing.LocalUserServiceTestConfig;
 import com.google.gson.JsonElement;
@@ -16,31 +11,25 @@ import com.google.sps.servlets.UserLoginServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-
 
 @RunWith(JUnit4.class)
 public class UserLoginTest {
 
-  @Mock
-  private HttpServletRequest request;
+  @Mock private HttpServletRequest request;
 
-  @Mock
-  private HttpServletResponse response;
+  @Mock private HttpServletResponse response;
 
-  @Mock
-  private UserLoginServlet userServlet;
+  @Mock private UserLoginServlet userServlet;
 
   private LocalServiceTestHelper helperLoggedIn =
     new LocalServiceTestHelper(new LocalUserServiceTestConfig())
@@ -51,14 +40,24 @@ public class UserLoginTest {
         .setEnvIsAdmin(true).setEnvIsLoggedIn(false);
     
   @Before
-  public void setUp() throws Exception  {
+  public void setUp() throws Exception {
     MockitoAnnotations.initMocks(this);
   }
 
   @Test
+<<<<<<< HEAD
   public void loggedInUserReturnsLogOutUrl() throws ServletException, IOException  {
 
     helperLoggedIn.setUp();
+=======
+  public void loggedInUserReturnsLogOutUrl() throws ServletException, IOException {
+    LocalServiceTestHelper helper =
+        new LocalServiceTestHelper(new LocalUserServiceTestConfig())
+            .setEnvIsAdmin(true)
+            .setEnvIsLoggedIn(true);
+    helper.setUp();
+
+>>>>>>> 56b0879... Google Java Format
     StringWriter stringWriter = new StringWriter();
     PrintWriter printWriter = new PrintWriter(stringWriter);
     when(response.getWriter()).thenReturn(printWriter);
@@ -68,11 +67,16 @@ public class UserLoginTest {
 
     String responseString = stringWriter.getBuffer().toString().trim();
     JsonElement responseJsonElement = new JsonParser().parse(responseString);
-         
+
     JsonObject responseJsonObject = responseJsonElement.getAsJsonObject();
     String logInUrl = responseJsonObject.get("LogInUrl").getAsString();
     String logOutUrl = responseJsonObject.get("LogOutUrl").getAsString();
+<<<<<<< HEAD
     
+=======
+
+    Assert.assertTrue(logInUrl.isEmpty());
+>>>>>>> 56b0879... Google Java Format
     Assert.assertFalse(logOutUrl.isEmpty());
     helperLoggedIn.tearDown();
   }
@@ -119,8 +123,6 @@ public class UserLoginTest {
     String logOutUrl = responseJsonObject.get("LogOutUrl").getAsString();
     
     Assert.assertFalse(logInUrl.isEmpty()) ;
-    // Assert.assertTrue(logOutUrl.isEmpty());
-    // Assert.assertTrue(logInUrl.contains("login"));
     helperLoggedOut.tearDown();
   }
    @Test
@@ -136,13 +138,12 @@ public class UserLoginTest {
 
     String response = stringWriter.getBuffer().toString().trim();
     JsonElement responseJsonElement = new JsonParser().parse(response);
-         
+
     JsonObject responseJsonObject = responseJsonElement.getAsJsonObject();
     String logInUrl = responseJsonObject.get("LogInUrl").getAsString();
     String logOutUrl = responseJsonObject.get("LogOutUrl").getAsString();
-    
+
     Assert.assertTrue(logInUrl.contains("login"));
     helperLoggedOut.tearDown();
   }
-
 }
