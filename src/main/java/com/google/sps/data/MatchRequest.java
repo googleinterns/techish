002f0cfa5650f0ab493cd29af3814fa8d23bd965
Samuel Sandoval <sjsandoval1000@gmail.com>
2020-06-26 23:06:50 +0000

    fixed merge conflicts

--- src/main/java/com/google/sps/data/MatchRequest.java
+++ src/main/java/com/google/sps/data/MatchRequest.java
@@ -1,23 +1,20 @@
 package com.google.sps.data;
 
-import java.util.ArrayList;
-import java.util.Collection;
 
 public final class MatchRequest {
-    //in the future, this will become a data structure but for MVP, we are just matching based on one criteria
-    private final String matchCriteria;
+  // in the future, this will become a data structure but for MVP, we are just matching based on one
+  // criteria
+  private final String matchCriteria;
 
-    public MatchRequest(String parameter) {
-        matchCriteria = parameter;
-    }
+  public MatchRequest(String parameter) {
+    matchCriteria = parameter;
+  }
 
-    public String getCriteria() {
-        return matchCriteria;
-    }
+  public String getCriteria() {
+    return matchCriteria;
+  }
 
-    public void changeCriteria(String newParameter) {
-        matchCriteria = newParameter;
-    }
-}
-  private final Collection<String> matchCriteria = new ArrayList();
+  public void changeCriteria(String newParameter) {
+    matchCriteria = newParameter;
+  }
 }
