b1d94e52d49357a3a15b566bcdf329e5cba9a0aa
Hadley <yeldah630@gmail.com>
2020-06-26 14:50:09 +0000

    alphabetize imports

--- src/test/java/com/google/sps/NewMatchQueryServletTest.java
+++ src/test/java/com/google/sps/NewMatchQueryServletTest.java
@@ -2,18 +2,18 @@ package com.google.sps;
 
 import static org.mockito.Mockito.when;
 
+import com.google.gson.Gson;
 import com.google.sps.algorithms.MatchQuery;
 import com.google.sps.data.MatchRequest;
-import com.google.sps.servlets.NewMatchQueryServlet;
-import com.google.gson.Gson;
 import com.google.sps.data.User;
+import com.google.sps.servlets.NewMatchQueryServlet;
 import java.io.IOException;
-import java.io.StringWriter;
 import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Collection;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Collection;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
