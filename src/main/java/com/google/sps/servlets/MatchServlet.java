4c6a501c148a2bd209221614a389820fb2388928
Hadley <yeldah630@gmail.com>
2020-06-30 21:39:47 +0000

    progress

--- src/main/java/com/google/sps/servlets/MatchServlet.java
+++ src/main/java/com/google/sps/servlets/MatchServlet.java
@@ -39,11 +39,15 @@ public class MatchServlet extends HttpServlet {
   public synchronized void doPost(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
 
-    String[] matchesToSave = request.getParameterValues("new-matches");
+    if(request.getParameterValues("new-matches") != null) {
 
-    for (String matchName : matchesToSave) {
-      User newMatch = new User(matchName);
-      testRepository.addMatch(testUser, newMatch);
+        String[] matchesToSave = request.getParameterValues("new-matches");
+
+        for (String matchName : matchesToSave) {
+        User newMatch = new User(matchName);
+        testRepository.addMatch(testUser, newMatch);
+        }
+
     }
 
     response.sendRedirect("/logged_in_homepage.html");
