acb572eba511e5429a1def877387f13120defc73
Samuel Sandoval <sjsandoval1000@gmail.com>
2020-07-06 19:55:50 +0000

    Added constructors in the User class

--- src/main/java/com/google/sps/data/User.java
+++ src/main/java/com/google/sps/data/User.java
@@ -5,12 +5,19 @@ import java.util.HashSet;
 
 public final class User {
 
+  private final long id;
+  private final String profileType;
   private final String name;
-  private final Collection<String> productArea;
+  private final String school;
+  private final String major;
+  private final String company;
+  private final String occupation;
+  private final String speciality;
+  private final Collection<String> specialties;
 
   public User(String name) {
     this.name = name;
-    productArea = new HashSet<String>();
+    specialties = new HashSet<String>();
   }
 
   public String toString() {
@@ -18,15 +25,34 @@ public final class User {
   }
 
   public boolean equals(User user) {
-    return this.name == user.name;
+    return this.id == user.id;
   }
 
-  public void addProductArea(String toAdd) {
-      productArea.add(toAdd);
+  public void addSpecialty(String toAdd) {
+      specialties.add(toAdd);
   }
 
-  public Collection<String> getProductAreas() {
-      return productArea;
+  public Collection<String> getSpecialties() {
+      return specialties;
+  }
+  
+   //Constructor for users that are students
+  public User(String profileType, long id, String name, String school, String major) {
+    this.profileType = profileType;
+    this.id = id;
+    this.name = name;
+    this.school = school;
+    this.major = major;
+  }
+
+ //Constructor for users that are professionals
+  public User(String profileType, long id, String name, String company, String occupation, String specialty) {
+    this.profileType = profileType;
+    this.id = id;
+    this.name = name;
+    this.company = company;
+    this.occupation = occupation;
+    this.specialty = speciality;
   }
 
 }
