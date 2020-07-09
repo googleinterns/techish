2873b72eae6859f777afdecf1459ba5f428577a8
Samuel Sandoval <sjsandoval1000@gmail.com>
2020-07-09 00:30:33 +0000

    Implement UserDatastoreRepository methods and tests

---
+++ src/main/java/com/google/sps/data/UserDatastoreRepository.java
@@ -0,0 +1,130 @@
+package com.google.sps.data;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.Gson;
+import com.google.sps.data.User;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+/**
+ * This class implements the match repository using the datastore 
+ */
+public class UserDatastoreRepository implements UserRepository {
+    
+  private Collection<User> allUsers = new ArrayList<>();
+  
+  private static final Gson gson = new Gson();
+
+  private DatastoreService datastore;
+  
+  private int maxProfiles = 10;
+  
+  public UserDatastoreRepository() {
+      datastore = DatastoreServiceFactory.getDatastoreService();
+  }
+
+  // function to add profile to database
+  public void addUserToDatabase(User user) {
+    String name = user.toString();
+    Collection<String> specialties = user.getSpecialties();
+
+    Entity userEntity = new Entity("User");
+    userEntity.setProperty("name", name);
+    if(specialties.size() > 0){
+        userEntity.setProperty("specialties", specialties);
+    }
+    datastore.put(userEntity);
+  }
+
+  // function to fetch the user from the database
+  public Collection<User> fetchUserProfiles() {
+    Query query = new Query("User");
+    PreparedQuery results = datastore.prepare(query);
+    List<Entity> resultsList = results.asList(FetchOptions.Builder.withLimit(maxProfiles));
+
+    Collection<User> userProfiles = new ArrayList<>();
+    for (Entity entity : resultsList) {
+        String name = (String) entity.getProperty("name");
+        Collection<String> specialties = (Collection) entity.getProperty("specialties");
+
+        User userObject = new User(name);
+        if(specialties != null) {
+            for (String specialty : specialties) {
+                userObject.addProductArea(specialty);
+            }
+        }
+        userProfiles.add(userObject);
+    }
+    return userProfiles;
+  }
+
+  public void removeUserProfile(User user){
+    Query query = new Query("User");
+    PreparedQuery results = datastore.prepare(query);
+    List<Entity> resultsList = results.asList(FetchOptions.Builder.withLimit(maxProfiles));
+
+    String name = user.toString();
+
+    for (Entity entity : resultsList) {
+        String currentName = (String) entity.getProperty("name");
+        if(name.contains(currentName)) {
+            Key current = entity.getKey();
+            datastore.delete(current);
+        }
+    }
+  }
+
+  public void addUser(User user) {
+    allUsers = fetchUserProfiles();
+
+    if(!allUsers.contains(user)) {
+        allUsers.add(user);
+        addUserToDatabase(user);
+    }
+  }
+
+  public void removeUser(User user) throws Exception {
+    if(allUsers.contains(user)) {
+        allUsers.remove(user);
+        removeUserProfile(user);
+    } else {
+        throw new Exception("Can't remove user that does not exist");
+    }
+  }
+
+  public Collection<User> getAllUsers() {
+    Collection<User> results = fetchUserProfiles();
+    return results;
+  }
+
+  public String toString() {
+    StringBuilder toReturn = new StringBuilder();
+    for(User user : allUsers) {
+        toReturn.append(user.toString());
+        toReturn.append(" ");
+    }
+    return toReturn.toString();
+  }
+
+}
---
+++ src/test/java/com/google/sps/UserDatastoreRepositoryTest.java
@@ -0,0 +1,228 @@
+package com.google.sps;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.gson.Gson;
+import com.google.sps.data.UserDatastoreRepository;
+import com.google.sps.data.User;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+/** */
+@RunWith(JUnit4.class)
+public final class UserDatastoreRepositoryTest { 
+    
+  @Mock private HttpServletRequest request;
+
+  @Mock private HttpServletResponse response; 
+
+  private LocalServiceTestHelper localHelper =
+    new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+
+  private DatastoreService dataService = DatastoreServiceFactory.getDatastoreService();
+
+  private static final User USER_A = new User("User A");
+  private static final User MATCH_A = new User("Match A");
+
+  public void addFakeMentors(UserDatastoreRepository input) {
+    User mentorA = new User("Andre");
+    mentorA.addProductArea("Machine Learning");
+    mentorA.addProductArea("DoS");
+
+    User mentorB = new User("Jerry");
+    mentorB.addProductArea("Electrical Engineering");
+    mentorB.addProductArea("DoS");
+
+    User mentorC = new User("Julie");
+    mentorC.addProductArea("Machine Learning");
+    mentorC.addProductArea("Security");
+
+    input.addUser(mentorA);
+    input.addUser(mentorB);
+    input.addUser(mentorC);
+    }
+
+  @Before
+  public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
+    localHelper.setUp();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    localHelper.tearDown();
+  }
+
+  @Test
+  public void addFakeMentorsTest() {
+    UserDatastoreRepository testDataRepo = new UserDatastoreRepository();
+    addFakeMentors(testDataRepo);
+    String expected = "Andre Jerry Julie ";
+    String result = testDataRepo.toString();
+    Assert.assertEquals(expected, result);
+  }
+
+  @Test
+  public void addUserTest() {
+    UserDatastoreRepository emptyRepo = new UserDatastoreRepository();
+    emptyRepo.addUser(USER_A);
+    String expected = "User A ";
+    String result = emptyRepo.toString();
+    Assert.assertEquals(expected, result);
+  }
+
+  @Test
+  public void removeUserThatExists() {
+    UserDatastoreRepository emptyRepo = new UserDatastoreRepository();
+    emptyRepo.addUser(USER_A);
+    try {
+      emptyRepo.removeUser(USER_A);
+      String expected = "";
+      String result = emptyRepo.toString();
+      Assert.assertEquals(expected, result);
+    } catch (Exception e) {
+      Assert.fail("Exception should not be thrown in removeUserThatExists");
+    }
+  }
+
+  @Test
+  public void removeUserThatDoesNotExist() {
+    UserDatastoreRepository emptyRepo = new UserDatastoreRepository();
+    try {
+      emptyRepo.removeUser(USER_A);
+      Assert.fail("Exception should be thrown in removeUserThatDoesNotExist()");
+    } catch (Exception e) {
+      // don't need to do anything here because test should catch exception
+    }
+  }
+
+  @Test
+  public void getAllUsersTest() {
+      User userA = new User("John");
+      User userB = new User("Bob");
+      User userC = new User("Haley");
+
+      UserDatastoreRepository myRepo = new UserDatastoreRepository();
+      myRepo.addUser(userA);
+      myRepo.addUser(userB);
+      myRepo.addUser(userC);
+
+      Collection<User> results = myRepo.getAllUsers();
+      int resultsLength = results.size();
+
+      Collection<User> expected = new ArrayList<User>();
+      expected.add(userA);
+      expected.add(userB);
+      expected.add(userC);
+      int expectedLength = expected.size();
+
+      Assert.assertEquals(resultsLength, expectedLength);
+  }
+
+  @Test
+  public void addSameUserMultipleTimes() {
+      User userA = new User("John");
+
+      UserDatastoreRepository myRepo = new UserDatastoreRepository();
+      myRepo.addUser(userA);
+      myRepo.addUser(userA);
+      myRepo.addUser(userA);
+
+      Collection<User> allUsers = myRepo.getAllUsers();
+      int actualSize = allUsers.size();
+
+      //myRepo should only have 1 user stored
+      Assert.assertEquals(1, actualSize);
+  }
+
+  @Test
+  public void multipleAddAndRemove() {
+      User userA = new User("John");
+      User userB = new User("Bob");
+
+      UserDatastoreRepository myRepo = new UserDatastoreRepository();
+      myRepo.addUser(userA);
+      myRepo.addUser(userB);
+
+      Collection<User> allUsers = myRepo.getAllUsers();
+      int currentSize = allUsers.size();
+
+
+      //myRepo should have 2 users stored
+      Assert.assertEquals(2, currentSize);
+
+      try {
+        myRepo.removeUser(userB);
+      } catch (Exception e) {
+          Assert.fail("Can't remove user that does not exist");
+      }
+      allUsers = myRepo.getAllUsers();
+      //myRepo should now only have 1 user stored
+      
+      currentSize = allUsers.size();
+      Assert.assertEquals(1, currentSize);
+
+      myRepo.addUser(userB);
+      allUsers = myRepo.getAllUsers();
+      currentSize = allUsers.size();
+      //myRepo should have 2 users stored again
+      Assert.assertEquals(2, currentSize);
+  }
+
+  @Test
+  public void singleAddAndGetUser() {
+      User userA = new User("Sundar");
+
+      UserDatastoreRepository myRepo = new UserDatastoreRepository();
+      myRepo.addUser(userA);
+      Collection<User> allUsers = myRepo.getAllUsers();
+      int currentSize = allUsers.size();
+
+      //myRepo should have 1 user stored
+      Assert.assertEquals(1, currentSize);
+  }
+
+  @Test
+  public void singleAddAndRemoveUser() {
+      User userA = new User("Sundar");
+
+      UserDatastoreRepository myRepo = new UserDatastoreRepository();
+      myRepo.addUser(userA);
+
+      try {
+        myRepo.removeUser(userA);
+      } catch (Exception e) {
+          Assert.fail("Can't remove user that does not exist");
+      }
+      Collection<User> allUsers = myRepo.getAllUsers();
+      int currentSize = allUsers.size();
+
+      //myRepo should have 0 users stored
+      Assert.assertEquals(0, currentSize);
+  }
+
+}
