a2e665829ab3cc6defb9065e43d9d926a066f04d
Samuel Sandoval <sjsandoval1000@gmail.com>
2020-07-10 22:11:22 +0000

    progress on front end of profile builder

---
+++ src/main/webapp/ts/profileBuilder.ts
@@ -0,0 +1,15 @@
+function setForm(value: any) {
+    const studentForm =  document.getElementById('student');
+    const professionalForm = document.getElementById('mentor')
+
+    if(studentForm && professionalForm){
+        if(value == 'student') {
+            studentForm.setAttribute('style','display:block;');
+            professionalForm.setAttribute('style','display:none;');
+        }
+        else {
+            professionalForm.setAttribute('style','display:block;');
+            studentForm.setAttribute('style','display:none;');
+        }
+    }
+} 
