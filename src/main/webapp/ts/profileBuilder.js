47d2cbc985113ccf1213f5d20747316be767fa70
Samuel Sandoval <sjsandoval1000@gmail.com>
2020-07-16 21:42:18 +0000

    made minor changes form for profileBuilder

--- src/main/webapp/ts/profileBuilder.js
+++ src/main/webapp/ts/profileBuilder.js
@@ -1,14 +1,17 @@
 function setForm(value) {
     var menteeForm = document.getElementById('Mentee');
     var mentorForm = document.getElementById('Mentor');
-    if (menteeForm && mentorForm) {
+    var userTypeButton = document.getElementById('user-input');
+    if (menteeForm && mentorForm && userTypeButton) {
         if (value == 'Mentee') {
             menteeForm.setAttribute('style', 'display:block;');
             mentorForm.setAttribute('style', 'display:none;');
+            userTypeButton.setAttribute('value', 'Mentee');
         }
         else {
             mentorForm.setAttribute('style', 'display:block;');
             menteeForm.setAttribute('style', 'display:none;');
+            userTypeButton.setAttribute('value', 'Mentor');
         }
     }
 }
--- src/main/webapp/ts/profileBuilder.ts
+++ src/main/webapp/ts/profileBuilder.ts
@@ -1,15 +1,18 @@
 function setForm(value: string) {
     const menteeForm =  document.getElementById('Mentee');
-    const mentorForm = document.getElementById('Mentor')
+    const mentorForm = document.getElementById('Mentor');
+    const userTypeButton = document.getElementById('user-input');
 
-    if(menteeForm && mentorForm){
+    if(menteeForm && mentorForm && userTypeButton){
         if(value == 'Mentee') {
             menteeForm.setAttribute('style','display:block;');
             mentorForm.setAttribute('style','display:none;');
+            userTypeButton.setAttribute('value','Mentee');
         }
         else {
             mentorForm.setAttribute('style','display:block;');
             menteeForm.setAttribute('style','display:none;');
+            userTypeButton.setAttribute('value','Mentor');
         }
     }
 } 
